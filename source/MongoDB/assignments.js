a2a86e04be57333b844e91e7818c7f7154f7c06a
technologic808 <20627603+technologic808@users.noreply.github.com>
2019-10-01 09:59:50 +0530

    Update assignments.js

--- source/MongoDB/assignments.js
+++ source/MongoDB/assignments.js
@@ -59,35 +59,71 @@ db.restaurants.find({cuisine: {$nin: ["American"]},'grades.score': {$gt: 70}, 'a
 // 'American ' and achieved a grade point 'A' not belongs to the borough Brooklyn. The
 // document must be displayed according to the cuisine in descending order.
 
-
+db.restaurants.find({cuisine:{$nin:['American']},'grades.grade':'A', borough:{$nin:["Brooklyn"]}});
 
 // 14. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which contain 'Wil' as first three letters for its name.
+
+db.restaurants.find({ name:/^Wil/}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty();
+
 // 15. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which contain 'ces' as last three letters for its name.
+
+db.restaurants.find({ name:/ces$/}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty();
+
 // 16. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which contain 'Reg' as three letters somewhere in its name.
+
+db.restaurants.find({ name:/Reg/}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty();
+
 // 17. Write a MongoDB query to find the restaurants which belong to the borough Bronx and
 // prepared either American or Chinese dish. 
+
+db.restaurants.find({ borough:'Bronx', cuisine: {$in :['American','Chinese']}}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty();
+
 // 18. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which belong to the borough Staten Island or Queens or Bronxor Brooklyn.
+
+db.restaurants.find({ borough: {$in :['Staten Island','Queens', 'Bronx', 'Brooklyn']}}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty();
+
 // 19. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which are not belonging to the borough Staten Island or Queens or Bronxor
 // Brooklyn.
+
+db.restaurants.find({ borough: {$nin :['Staten Island','Queens', 'Bronx', 'Brooklyn']}}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id:0}).pretty();
+
 // 20. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which achieved a score which is not more than 10.
+
+// A Score
+db.restaurants.find({ "grades.score": {$lt: 10} }, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1, "grades.score":1,  _id:0}).pretty();
+
+// All Scores #TODO
+
 // 21. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those
 // restaurants which prepared dish except 'American' and 'Chinees' or restaurant's name begins
 // with letter 'Wil'.
+
+db.restaurants.find({cuisine:{$nin:['American', 'Chinese']}},{restaurant_id:1, _id:0, borough:1, cuisine:1}).pretty()
+
 // 22. Write a MongoDB query to find the restaurant Id, name, and grades for those restaurants
 // which achieved a grade of "A" and scored 11 on an ISODate "2014-08-11T00:00:00Z"
 // among many of survey dates
+
+db.restaurants.find({ 'grades.grade':'A', grades:{$elemMatch:{grade:"A", date: ISODate("2014-08-11T00:00:00Z") }}}, {restaurant_id:1, _id:0, name:1, 'grades.grade':1}).pretty()
+
 // 23. Write a MongoDB query to find the restaurant Id, name and grades for those restaurants
 // where the 2nd element of grades array contains a grade of "A" and score 9 on an ISODate
 // "2014-08-11T00:00:00Z".
+
+db.restaurants.find({ 'grades.1.grade':'A', grades:{$elemMatch:{grade:"A", date: ISODate("2014-08-11T00:00:00Z") }}}, {restaurant_id:1, _id:0, name:1, 'grades.grade':1}).pretty()
+
 // 24. Write a MongoDB query to find the restaurant Id, name, address and geographical
 // location for those restaurants where 2nd element of coord array contains a value which is
 // more than 42 and upto 52
+
+db.restaurants.find({'address.coord.1':{$gt:42, $lt:52}}, {restaurant_id:1, _id:0, name:1, address:1}).pretty()
+
 // 25. Write a MongoDB query to arrange the name of the restaurants in ascending order along
 // with all the columns.
 // 26. Write a MongoDB query to arrange the name of the restaurants in descending along with
